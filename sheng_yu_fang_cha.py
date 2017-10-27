be99ade6469f0d17f1185b426ba22e71e988340a
HDTTclear <980958644@qq.com>
2017-10-27 16:51:47 +0800

    Update sheng_yu_fang_cha.py

--- sheng_yu_fang_cha.py
+++ sheng_yu_fang_cha.py
@@ -13,16 +13,28 @@ def polyfit(x, y, degree):#线性回归代码
     results['determination'] = ssreg / sstot #准确率
     return results#使用方式：返回值是一个集合，包括'polynomial'，是一个含有斜率和截距的列表 'determination'是置信度
 #在这里把x,y数据改了
-
-y=[104.267	,95.350	,86.570	,77.978,	69.208	,60.480	,51.805	,42.969	,34.032,	25.152]
 x=[0,1,2,3,4,5,6,7,8,9]
-z1 = polyfit(x, y, 1)
-line=[z1['polynomial'][0],z1['polynomial'][1]]#line是一个保存斜率和截距的列表
-print('回归曲线的斜率和截距',line)#
+y=[20.630,29.665,	38.620	,47.513	,56.371	,65.118,	73.613,	82.430	,91.219	,100.001]
+#y=[30.432,	34.892	,39.242,	43.609,	48.290	,52.513,	56.893,	61.252	,65.750,	70.153]
+#y=[6.96	,6.48,	6.24,	6.08,	5.76,	5.28,	4.88	,4.48,	4.16	,3.68]
+
+n=1#n是你要拟合几次方函数
+z1 = polyfit(x, y, n)
+line=[]
+for i in range(n+1):
+    line.append(z1['polynomial'][i])
+#line=[,z1['polynomial'][1]]#line是一个保存由n次到0次系数的列表
+print('回归曲线的信息',line)#
 
 sum=0
+
 for i in range(len(x)):
-    sum=sum+(y[i]-line[1]-line[0]*x[i])*(y[i]-line[1]-line[0]*x[i])
+    h=0
+    for j in range(len(line)):#line列表的长度=n+1
+        h=h+line[j]*pow(x[i],(len(line)-j-1))#根据拟合曲线算出来的那个值
+
+
+    sum=sum+(y[i]-h)*(y[i]-h)#算剩余方差
 result=sum/(len(x)-2) #剩余方差
 print("剩余方差是",result);
 def average(x):#平均值
@@ -37,4 +49,4 @@ print(sum3)
 print(average(x))
 #for i in y:
 xigema_a1= math.sqrt(result / sum3)#这是斜率的不确定度
-print("斜率的不确定度是",xigema_a1)
+print("斜率的不确定度是",xigema_a1)#是给一次式算的，高次勿用
